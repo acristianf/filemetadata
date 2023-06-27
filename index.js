92d99d2d3f553fdf2080121f8ab8f383aa9b1607
acristianf <acristianf@hotmail.com>
2023-06-26 23:28:52 -0300

    finished functionality

--- index.js
+++ index.js
@@ -1,20 +1,34 @@
 var express = require('express');
 var cors = require('cors');
-require('dotenv').config()
+var multer = require('multer');
+require('dotenv').config();
 
 var app = express();
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.get('/', function (req, res) {
+const storage = multer.diskStorage({
+  destination: (_, __, cb) => {
+    cb(null, 'uploads/');
+  },
+  filename: (_, file, cb) => {
+    cb(null, Date.now() + '-' + file.originalname);
+  }
+});
+const upload = multer({ storage: storage })
+
+app.get('/', function(_, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-
-
+app.post('/api/fileanalyse', upload.single('upfile'), async (req, res) => {
+  res.json({ name: req.file.filename, type: req.file.mimetype, size: req.file.size });
+});
 
 const port = process.env.PORT || 3000;
-app.listen(port, function () {
+app.listen(port, function() {
   console.log('Your app is listening on port ' + port)
 });
